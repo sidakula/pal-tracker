a2ac96130f083039e3ec6407b03acc5d59702d50
siddardha akula <e070087@mastercard.com>
2018-01-31 16:59:56 -0500

    jdbc template

--- src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
@@ -2,12 +2,13 @@ package test.pivotal.pal.tracker;
 
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import io.pivotal.pal.tracker.JdbcTimeEntryRepository;
 import io.pivotal.pal.tracker.TimeEntry;
 import io.pivotal.pal.tracker.TimeEntryRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
+import io.pivotal.pal.tracker.JdbcTimeEntryRepository;
+
 
 import java.sql.Date;
 import java.time.LocalDate;
